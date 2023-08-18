b00fb10ad31854446852e78f3189fad2d8622457
zezang <zezang@gmail.com>
2023-08-17 21:36:22 -0700

    Added new dependencies and created app and index

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+
+const App: React.FC = () => {
+  return (
+    <div>Welcome to Tourista</div>
+  )
+};
+
+
+
+export default App;
---
+++ src/index.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import App from "App";
+
+const container = document.getElementById("root");
+
+ReactDOM.render(
+
+)
+
