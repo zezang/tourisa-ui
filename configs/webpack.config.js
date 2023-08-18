3605cca8de1d32ad6de0225ebdb6d77f7bd5cb7e
zezang <zezang@gmail.com>
2023-08-17 22:18:32 -0700

    Added addition eslint configs

--- configs/webpack.config.js
+++ configs/webpack.config.js
@@ -1,3 +1,9 @@
 "use strict";
 
 const path = require("path");
+
+module.exports = ((mode) => {
+  const isProduction = mode === "production";
+
+  
+})
--- src/index.tsx
+++ src/index.tsx
@@ -1,10 +1,11 @@
 import React from "react";
-import ReactDOM from "react-dom";
+import { createRoot } from "react-dom/client";
 import App from "App";
 
 const container = document.getElementById("root");
+const root = createRoot(container as Element);
 
-ReactDOM.render(
-
+root.render(
+  <App />
 )
 
