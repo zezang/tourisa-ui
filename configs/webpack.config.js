88da48ff4980b2aa67af939f5175b118714cea10
zezang <zezang@gmail.com>
2023-08-19 22:35:10 -0700

    Finished eslint for now

--- configs/webpack.config.js
+++ configs/webpack.config.js
@@ -1,9 +1,17 @@
 "use strict"
 
+const fs = require("fs");
 const path = require("path");
 
-module.exports = ((mode) => {
-  const isProduction = mode === "production";
+module.exports = ((env) => {
+  const isProduction = env === "production";
 
-  
-})
+  return {
+    entry: "./src/index.js",
+    output: {
+      path: path.resolve(__dirname, "build"),
+      filename: "bundle.js",
+      publicPath: "/",
+    }
+  };
+});
