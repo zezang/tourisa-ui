a27857c00bf280b10edd210fc05b19ff2f91b52a
zezang <zezang@gmail.com>
2023-08-20 22:28:54 -0700

    finished get styles loader function

--- configs/webpack.config.js
+++ configs/webpack.config.js
@@ -17,7 +17,7 @@ module.exports = ((env) => {
   const isDevelopment = env !== "production";
   const shouldRunBundleAnalyzer = process.argv.includes("--analyzer");
 
-  const getStyleLoaders = (cssOptions, preprocessor) => {
+  const getStyleLoaders = (cssOptions, preProcessor) => {
     const loaders = [
       isDevelopment && require.resolve("style-loader"),
       isProduction && {
@@ -29,16 +29,60 @@ module.exports = ((env) => {
         options: cssOptions,
       },
       {
-        loader: require.resolve("postcss-laoder"),
+        loader: require.resolve("postcss-loader"),
         options: {
-
+          postcssOptions: {
+            ident: "postcss",
+            plugins: [
+              "postcss-flexbugs-fixes",
+              [
+                "postcss-preset-env", 
+                { 
+                  autoprefixer: { 
+                    flexbox: "no-2009" 
+                  },
+                  stage: 3,
+                },
+              ],
+              // Makes sure default styles are consistent across browsers
+              "postcss-normalize",
+              // Adds ability to reference assets in scss files from a base path
+              // background-image: url(assets/**)
+              [
+                "postcss-url",
+                {
+                  basePath: paths.publicUrlOrPath,
+                },
+              ],
+              // Allows use of overflow shorthand syntax such as "overflow: hidden scroll"
+              "postcss-overflow-shorthand"
+            ],
+          },
         },
       },
     ].filter(Boolean);
 
+    if (preProcessor) {
+      loaders.push(
+        {
+          loader: require.resolve("resolve-url-loader"),
+          options: {
+            soureMap: isDevelopment,
+            root: paths.appSrc
+          },
+        },
+        {
+          loader: require.resolve(preProcessor),
+          options: { sourceMap: true },
+        },
+      );
+    };
   };
 
   return {
+    target: ["browserslist"],
+    bail: isProduction,
+    devtool: isProduction ? false : "eval",
     // Entry point for the application. Should be *root/src/index.[js|ts]
     entry: paths.appIndexJs,
     output: {
@@ -53,20 +97,22 @@ module.exports = ((env) => {
       minimize: isProduction,
       minimizer: [
         new TerserPlugin({
-          compress: {
-            ecma: 5,
-            comparisons: false,
-            inline: 2,
-            warnings: false,
-          },
-          mangle: {
-            safari10: true,
-          },
-          output: {
-            ecma: 5,
-            comments: false,
-            ascii_only: true,
-          },
+          terserOptions: {
+            compress: {
+              ecma: 5,
+              comparisons: false,
+              inline: 2,
+              warnings: false,
+            },
+            mangle: {
+              safari10: true,
+            },
+            output: {
+              ecma: 5,
+              comments: false,
+              ascii_only: true,
+            },
+          }
         }),
         new CssMinimizerPlugin(),
       ],
