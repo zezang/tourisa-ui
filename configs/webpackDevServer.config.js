3d3b3a39d019f3b8ae70219347b24c3dbf01e321
zezang <zezang@gmail.com>
2023-08-27 21:44:49 -0700

    Finished dev server config

--- configs/webpackDevServer.config.js
+++ configs/webpackDevServer.config.js
@@ -1,2 +1,58 @@
 'use strict';
 
+const fs = require("fs");
+const evalSourceMapMiddlware = require("react-dev-utils/evalSourceMapMiddleware");
+const ignoredFiles = require("react-dev-utils/ignoredFiles");
+const noopServiceWorkerMiddlware = require("react-dev-utils/noopServiceWorkerMiddleware");
+const redirectServedPath = require("react-dev-utils/redirectServedPathMiddleware");
+
+const getHttpsConfig = require("./getHttpsConfig");
+const paths = require("./paths");
+
+const host = process.env.HOST || "0.0.0.0";
+
+module.exports = function(proxy, allowedHost) {
+  return {
+    allowedHosts: proxy ? [allowedHost] : "all",
+    client: {
+      overlay: {
+        warnings: false,
+        errors: true,
+      },
+    },
+    devMiddleware: {
+      publicPath: paths.publicUrlOrPath.slice(0, -1),
+    },
+    historyApiFallback: {
+      disableDotRule: true,
+      index: paths.publicUrlOrPath,
+    },
+    host,
+    hot: true,
+    https: getHttpsConfig(),
+    static: {
+      directory: paths.appPublic,
+      publicPath: [paths.publicUrlOrPath],
+      watch: {
+        ignored: ignoredFiles(paths.appSrc),
+      },
+    },
+    setupMiddlewares(middlewares, devServer) {
+      if (!devServer) {
+        throw new Error("webpack-dev-server is not defined");
+      }
+
+      if (fs.existsSync(paths.proxySetup)) {
+        require(paths.proxySetup)(devServer.app)
+      }
+
+      middlewares.unshift(
+        evalSourceMapMiddlware(devServer),
+        redirectServedPath(paths.publicUrlOrPath),
+        noopServiceWorkerMiddlware(paths.publicUrlOrPath),
+      );
+
+      return middlewares;
+    },
+  };
+};
