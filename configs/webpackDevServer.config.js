5106012fa57a60003a9014bfbdb19b2b3f7725e3
zezang <zezang@gmail.com>
2023-08-27 21:50:33 -0700

    edit some configs

--- configs/webpackDevServer.config.js
+++ configs/webpackDevServer.config.js
@@ -1,4 +1,4 @@
-'use strict';
+"use strict";
 
 const fs = require("fs");
 const evalSourceMapMiddlware = require("react-dev-utils/evalSourceMapMiddleware");
---
+++ scripts/start.js
@@ -0,0 +1,4 @@
+"use strict";
+
+process.env.BABEL_ENV = "development";
+process.env.NODE_ENV = "development";
